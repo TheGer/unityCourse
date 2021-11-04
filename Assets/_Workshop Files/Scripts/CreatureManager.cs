88a92510f7c6fe9bc11f2e9075acd0387d8461a4
Gerard Said <Gerard.Said@mcast.edu.mt>
2021-11-04 18:23:14 +0100

    lesson

---
+++ Assets/_Workshop Files/Scripts/CreatureManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureManager : MonoBehaviour
+{
+    public GameManager gameManager;      // Reference to the player's heatlh.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    public GameObject[] enemies;     // An array of possible enemies.
+
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        // If the player has no health left...
+        if (!gameManager.playerAlive)
+        {
+            // ... exit the function.
+            return;
+        }
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        GameObject enemy = Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+
+
+    }
+}
---
+++ gitcommit.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "lesson"
+git push
